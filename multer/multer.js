8df5bf56e208bc134d39bbd94fd4a82262e327bd
guiinfo3333 <antonioguilhermeinfo@gmail.com>
2021-08-28 21:26:13 -0500

    ajeitando rota

--- multer/multer.js
+++ multer/multer.js
@@ -8,8 +8,17 @@ module.exports = {
         filename(request, file, callback) {
             console.log(file)
             const hash = crypto.randomBytes(16).toString('hex');
-            const filename = `${hash}-${file.originalname}`;
+            const filename = `${hash}-${ajeitaEspaco(file.originalname)}`;
             callback(null, filename);
         }
     }),
 };
+
+function ajeitaEspaco(originalname) {
+    var array = originalname.split(" ")
+    var nomenovo = ""
+    for (var i = 0; i < array.length; i++) {
+        nomenovo = nomenovo + "_" + array[i]
+    }
+    return nomenovo
+}
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const cors = require('cors')
 const app = express()
+const path = require('path')
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use('/uploads', express.static(path.resolve(__dirname, 'uploads')));
 const multer = require('multer');
 const multerConfig = require('./multer/multer');
 const upload = multer(multerConfig);
