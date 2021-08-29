3e62ae23bd8b934d28e91e116e13c02baeced154
guiinfo3333 <antonioguilhermeinfo@gmail.com>
2021-08-28 21:41:08 -0500

    ajeitando

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require('cors')
 const app = express()
 const path = require('path')
+const { unlink } = require('fs/promises');
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -14,6 +15,22 @@ const upload = multer(multerConfig);
 app.post('/enviararquivo', upload.single('image'), async function (req, res) {
     if (req.file.filename != "") {
         return res.status(200).json({ "data": "Sucesso !" })
+    } else {
+        return res.status(400).json({ "data": "Ocorreu algum erro !" })
+    }
+})
+
+app.delete('/apagararquivo', async function (req, res) {
+    var nome = req.body.arquivo
+    if (nome != "") {
+        try {
+            await unlink(path.resolve(__dirname, 'uploads', nome))
+            return res.status(200).json({ "data": "Sucesso !" })
+
+        } catch (err) {
+            console.log(err)
+            return res.status(400).json({ "data": "Ocorreu algum erro !" })
+        }
     } else {
         return res.status(400).json({ "data": "Ocorreu algum erro !" })
     }
